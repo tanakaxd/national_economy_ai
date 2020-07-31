6a1b2d43cff76a3030a66e67873162fcabd03964
tanakaXD <ttnk.0112@gmail.com>
2020-07-31 11:52:38 +0900

    before checkout. branching for AI overhaul

--- NE/player/ai/IAI.java
+++ NE/player/ai/IAI.java
@@ -13,7 +13,18 @@ public interface IAI {
     // ターン終了時に捨てるカードを選ぶ。戻り値は手札内のindex
     public abstract int discard(Player self, Board board);
 
+    // public abstract int discard(Player self, Board board, int indexesNotAllowed);
+
     // 賃金支払いのため売却する物件を選ぶ。戻り値は自物件内のindex
     public abstract int sell(Player self, Board board);
 
 }
+
+/*
+ * 
+ * AIの方針： AIに連続的な選択を意識させたかったから、一連の選択を受け取るという目的で戻り値をリストで作ったが、その必要があるのか疑問
+ * その都度「使いたいカード」「建てたいカード」「捨てたいカード」「売りたいカード」という分解された問いを投げかける構造にしてもいいかもしれない
+ * つまり、その問い一つ一つに対応したメソッドをAI側は持っていて、カードの効果はその組み合わせで実現するということ
+ * 例えば、建築カードなら「建てたいカード」を聞いて、そのコスト分だけ「捨てたいカード」を聞くという設計
+ * 
+ */
